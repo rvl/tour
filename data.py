148127c4322ef9663e062943b72680db666d7533
Rodney Lorrimar <rodney@rodney.id.au>
2012-08-01 23:29:59 +0200

    Add locations of stops

--- data.py
+++ data.py
@@ -7,19 +7,21 @@ import sys
 import csv
 
 class TourDay(object):
-    def __init__(self, row, prev_location=None):
-        self.from_row(row, prev_location)
+    def __init__(self, row, prev_day=None):
+        self.from_row(row, prev_day)
 
-    def from_row(self, row, prev_location=None):
+    def from_row(self, row, prev_day=None):
         self.date = self.parse_date(row[0])
-        self.start = prev_location
-        self.finish = row[1] or prev_location
+        self.start = prev_day.finish if prev_day else None
+        self.finish = row[1] or self.start
         self.start_time = self.parse_time(row[2])
         self.finish_time = self.parse_time(row[3])
         self.dist = float(row[4]) if row[4] else 0.0
         self.accom = row[5]
         self.transport = row[6]
         self.number = row[7]
+        self.lat = row[8] or (prev_day.lat if prev_day else None)
+        self.lon = row[9] or (prev_day.lon if prev_day else None)
 
     @staticmethod
     def parse_date(text):
@@ -45,6 +47,8 @@ class TourDay(object):
             "accom": self.accom,
             "transport": self.transport,
             "number": self.number,
+            "lat": self.lat,
+            "lon": self.lon,
         }
 
     def gpx_path(self):
@@ -56,34 +60,33 @@ class TourDay(object):
     @classmethod
     def csv_header(cls, writer):
         writer.writerow(["Date", "Place", "Start Time", "Finish Time",
-                         "Dist", "Accom", "Transport", "Number"])
+                         "Dist", "Accom", "Transport", "Number", "Lat", "Lon"])
 
     def csv_row(self, writer):
         writer.writerow([self.strdate(), self.finish,
                          self.start_time.strftime("%H:%M") if self.start_time else None,
                          self.finish_time.strftime("%H:%M") if self.finish_time else None,
-                         self.dist, self.accom, self.transport, self.number])
+                         self.dist, self.accom, self.transport, self.number,
+                         self.lat, self.lon])
 
 def read_table(table):
     days = []
-    last_location = None
+    last_day = None
     for row in table:
-        day = TourDay(row, last_location)
-        days.append(day)
-        last_location = day.finish
+        last_day = TourDay(row, last_location)
+        days.append(last_day)
     return days
 
 #data = read_table(table)
 
 def read_csv(filename):
     days = []
-    last_location = None
+    last_day = None
     reader = csv.reader(open(filename))
-    next(reader)
+    next(reader) # skip header row
     for row in reader:
-        day = TourDay(row, last_location)
-        days.append(day)
-        last_location = day.finish
+        last_day = TourDay(row, last_day)
+        days.append(last_day)
     return days
 
 data = read_csv(os.path.join(os.path.dirname(__file__), "tour2012.csv"))
